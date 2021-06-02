92de7b7ff854fbe18c0afcc51a7bc2b75e97258a
axelaero <axelyudhiputra@gmail.com>
2021-06-02 08:42:43 +0700

    approved atatus on kelurahan dashboard deleted

--- resources/views/kelurahan/pendudukdashboard.blade.php
+++ resources/views/kelurahan/pendudukdashboard.blade.php
@@ -11,7 +11,7 @@
                     <td class="text-center border px-10 py-2">Nama</td>
                     <td class="text-center border px-8 py-2">Alamat</td>
                     <td class="text-center border px-10 py-2">status</td>
-                    <td class="text-center border px-10 py-2">approved</td>
+                    <!-- <td class="text-center border px-10 py-2">approved</td> -->
                     <!-- <td class="text-center border px-10 py-2">periode</td> -->
                 </tr>
                 <?php
@@ -46,11 +46,11 @@
                             <a href='/pendudukupdate/{{$dt->penduduk_id}}' class='btn-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-green-700 hover:bg-green-900 text-white font-normal py-0.5 px-4 mr-1 rounded'>Update</a>
                         </td>
                     @endif
-                    @if($dt->approved_deskripsi)
+                    <!-- @if($dt->approved_deskripsi)
                         <td class="text-center border px-10 py-2">{{$dt->approved_deskripsi}}</td>
                     @else
                         <td class="text-center border px-10 py-2">Belum di cek</td>
-                    @endif
+                    @endif -->
                     <!-- <td class="text-center border px-10 py-2">{{$dt->periode}}</td> -->
                     
                 </tr>
